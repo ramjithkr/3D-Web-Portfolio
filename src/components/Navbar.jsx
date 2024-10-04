85494d67605f3170de7056e9ab47fd6d9412a6f9
ramjithkr <ramjithkr5441@gmail.com>
2024-10-05 02:41:43 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import { div, nav } from "framer-motion/client";
+import { styles } from "../styles";
+
+const Navbar = () => {
+  const [active, setActive] = useState();
+  return (
+    <nav
+      className={`${styles.paddingX}  w-full flex items-center py-5 fixed top-0 z-20`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo"
+          className="w-9 h-9 object-contain" />
+
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
