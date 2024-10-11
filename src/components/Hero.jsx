67c08a30b9ab84c4e58cbd970ce9d9a2a4c8f813
ramjithkr <ramjithkr5441@gmail.com>
2024-10-12 01:39:50 +0530

    major commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Ramjith</span>
+            Hi, I&apos;m <span className="text-[#915eff]">Ramjith</span>
           </h1>
           <p className={`${styles.heroSubText}mt-2 text-white-100`}>
             I’m a full-stack MERN developer skilled in building efficient web
@@ -27,6 +27,23 @@ const Hero = () => {
       
       </div>
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,18 +1,12 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF, Html } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const CanvasLoader = () => {
-  return (
-    <Html center>
-      <div style={{ color: "white" }}>Loading...</div>
-    </Html>
-  );
-};
+import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
+  
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
