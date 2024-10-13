0fe997b0d91a222c8fc079847d649cf216ab7a00
ramjithkr <ramjithkr5441@gmail.com>
2024-10-13 15:49:24 +0530

    finnal commit -m

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,9 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//template_hvrmw2t
+//service_mdylwer
+//qs7brVPxv4E4xjmj3
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -37,9 +40,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Ramjith kr",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "ramjithkr5441@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
